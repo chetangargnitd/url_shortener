5a66aeb46dc4d09b382505462cfbee4e408aac58
chetangargnitd <chetangarg1102@gmail.com>
2019-06-13 17:06:24 +0530

    user can customise url

--- shorten/views.py
+++ shorten/views.py
@@ -15,22 +15,28 @@ def home(request):
 	return render(request, 'shorten/index.html')
 
 
-
 def shrink(request):
 	url = (request.GET["url"])
-	# print("1 :{} ".format(url))
+	c_url = (request.GET["c_url"])
+	print(type(c_url))
 	encoded_url = url.encode('utf-8')
-	# print("2 : ", url)
 	hashObject = hashlib.md5(encoded_url)
 	shrinked_url = hashObject.hexdigest()[:8]
 
-	try:
-		check = URLs.objects.get(shrinked_url = shrinked_url)
-	except URLs.DoesNotExist:
-		entry = URLs(shrinked_url = shrinked_url, original_url = url)
-		entry.save()
-	return render(request, 'shorten/index.html', {'shrinked_url' : shrinked_url})
-
+	if(c_url == ""):
+		try:
+			check = URLs.objects.get(shrinked_url = shrinked_url)
+		except URLs.DoesNotExist:
+			entry = URLs(shrinked_url = shrinked_url, original_url = url)
+			entry.save()
+		return render(request, 'shorten/index.html', {'shrinked_url' : shrinked_url})
+	else:
+		try:
+			check = URLs.objects.get(shrinked_url = c_url)
+		except URLs.DoesNotExist:
+			entry = URLs(shrinked_url = c_url, original_url = url)
+			entry.save()
+		return render(request, 'shorten/index.html', {'shrinked_url' : c_url})
 
 def retrieve(request, id):
 	target = get_object_or_404(URLs, shrinked_url = id)
